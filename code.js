a0a707ecb463ad78fa4cd2ba8f444d83a81209cd
nisreenissa <nisreentayseer93@hotmail.com>
2020-12-23 16:34:28 +0200

    ms

---
+++ code.js
@@ -0,0 +1,15 @@
+var username =prompt("please enter your name");
+    alert('Thank you for visting us '+username );
+
+    var dish =prompt("please enter your favourite dish ");
+    alert('Thank you for visting us '+dish );
+
+    var age =prompt("please enter your age");
+    alert('Thank you for visting us '+ age );
+
+    if (age < 14){
+    alert('Please make sure to ask your dad  before trying to buy from his credit card!');
+}
+else if (age > 14){
+alert('you can buy what ever you want and enjoy it !');
+}
